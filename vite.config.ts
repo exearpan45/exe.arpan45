9036bb721ab3690edfaacd5925a63adcfa4100c9
exearpan45 <exe.arpan45@gmail.com>
2026-08-15 10:25:18 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/exearpan45/'
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
